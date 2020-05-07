c33039b9b59ea3669912ca54ee10762c4b40e626
Steve Drake <sdrake@trueNorthIT.co.uk>
2020-05-07 09:34:08 +0100

    refactor

--- src/app/ride-indexer.service.ts
+++ src/app/ride-indexer.service.ts
@@ -3,9 +3,9 @@ import Dexie from 'dexie';
 import { DexieService } from './dexie.service';
 import { ActivitiesService, SegmentsService } from './api/api';
 import { flatten } from '@angular/compiler';
-import { Observable, observable, combineLatest, forkJoin } from 'rxjs';
-import { DetailedActivity } from './model/models';
-import { map } from 'rxjs/operators';
+import { Observable, observable, combineLatest, forkJoin, from, Subscriber } from 'rxjs';
+import { DetailedActivity, SummaryActivity } from './model/models';
+import { map, mergeMap, delay } from 'rxjs/operators';
 
 export interface Activity {
   activity: number;
@@ -63,48 +63,46 @@ export class RideIndexerService {
   }
 
   indexOb(): Observable<string> {
-
     const simpleObservable = new Observable<string>((observer) => {
-      // observable execution
-
       const allSearch = new Array<Observable<any>>();
-
       observer.next('loading rides');
-      const a = this.activitiesService.getLoggedInAthleteActivities(null, null, null, 100).subscribe((activities) => {
+      this.activitiesService.getLoggedInAthleteActivities(null, null, 1, 6).subscribe((activities) => {
         console.log('loaded rides');
-        for (const activitySummary of activities.slice(1, 20)) {
-
-          this.activityKeywords.where('activity').equals(activitySummary.id).delete().then((d) => console.log('Deleting main row', d));
-          this.activities.where('activity').equals(activitySummary.id).delete().then((d) => console.log('Deleting words', d));
-
-          const sub =
-            this.activitiesService.getActivityById(activitySummary.id).pipe(map((activity) => {
-
-              let tempArray = activity.name.toLowerCase().match(/\b(\w+)\b/g);
-              for (const segment of activity.segment_efforts) {
-                if (segment.name) {
-                  tempArray = tempArray.concat(segment.name.toLowerCase().match(/\b(\w+)\b/g));
-                }
-                if (segment.segment.city) { tempArray = tempArray.concat(segment.segment.city.toLowerCase().match(/\b(\w+)\b/g)); }
-              }
-              tempArray = tempArray.filter(this.onlyUnique);
-              for (const word of tempArray) {
-                this.activityKeywords.add({ activity: activity.id, word });
-              }
-              this.activities.add({ activity: activity.id, title: activity.name });
-
-              console.log('Indexing', activity.name);
-              observer.next('Indexing ' + activity.name);
-            }));
-
-          // sub.subscribe();
-
-          allSearch.push(sub);
+        from(activities).pipe(mergeMap((activitySummary) => {
+          return this.loadActivity(activitySummary, observer);
         }
-        forkJoin(allSearch).subscribe(() =>  observer.next('All rides loaded, whoop whoop (well a sample have been)') );
+          , 2)).subscribe(
+            (s) => { }, (err) => console.error(err), () => observer.next('All rides loaded, whoop whoop (well a sample have been)')
+          );
       });
     });
     return simpleObservable;
+  }
+
+  private loadActivity(activitySummary: SummaryActivity, observer: Subscriber<string>): Observable<void> {
+    console.log('foreach');
+    this.activityKeywords.where('activity').equals(activitySummary.id).delete();
+    this.activities.where('activity').equals(activitySummary.id).delete();
+    const sub = this.activitiesService.getActivityById(activitySummary.id).pipe(delay(1000), map((activity) => {
+      console.log('callback');
+      let tempArray = activity.name.toLowerCase().match(/\b(\w+)\b/g);
+      for (const segment of activity.segment_efforts) {
+        if (segment.name) {
+          tempArray = tempArray.concat(segment.name.toLowerCase().match(/\b(\w+)\b/g));
+        }
+        if (segment.segment.city) {
+          tempArray = tempArray.concat(segment.segment.city.toLowerCase().match(/\b(\w+)\b/g));
+        }
+      }
+      tempArray = tempArray.filter(this.onlyUnique);
+      for (const word of tempArray) {
+        this.activityKeywords.add({ activity: activity.id, word });
+      }
+      this.activities.add({ activity: activity.id, title: activity.name });
+      console.log('Indexing', activity.name);
+      observer.next('Indexing ' + activity.name);
+    }));
+    return sub;
   }
 
   async find(term): Promise<Activity[]> {
@@ -118,9 +116,7 @@ export class RideIndexerService {
       prefixes.map(prefix =>
         this.activityKeywords.where('word').startsWith(prefix).each((x) => {
           result.push(x.activity);
-        }
-        )
-      ));
+        })));
 
     console.log('result', result);
     console.log('result by count', this.byCount(result));
@@ -137,13 +133,10 @@ export class RideIndexerService {
 
   byCount(array) {
     const frequency = {};
-
     array.forEach((value) => { frequency[value] = 0; });
-
     const uniques = array.filter((value) => {
       return ++frequency[value] === 1;
     });
-
     console.log('uniques', uniques);
     console.log('frequency', frequency);
     return uniques.filter((f) => frequency[f] > 0).sort((a, b) => frequency[b] - frequency[a]);
