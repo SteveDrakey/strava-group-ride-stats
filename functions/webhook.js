55196d3ba707715b683cd31a88f689381e6d4111
Steve Drake <sdrake@trueNorthIT.co.uk>
2025-02-06 12:34:41 +0000

    Update webhook.js

--- functions/webhook.js
+++ functions/webhook.js
@@ -109,6 +109,11 @@ function makeNiceName(token, activityId, updateName) {
       if (activity.type == 'Run' ) return;
       if (activity.type !== 'Ride') return;
 
+      if (activity.name && activity.name.toUpperCase().includes("XERT")) {
+        console.log('Activity name contains "XERT", skipping');
+        return;
+      }
+
       if (!updateName && activity.name && activity.name.length > 3 && activity.description && activity.description.length > 3) {
         console.log('We have a name and desc, skipping');
         return;
